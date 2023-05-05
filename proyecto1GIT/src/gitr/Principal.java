9ca1293d8f843b6c7b1b520a4eb6f365aa10175f
Diurno <Diurno@pc0105>
2023-05-05 13:43:28 +0200

    Tercer cambio

--- proyecto1GIT/src/gitr/Principal.java
+++ proyecto1GIT/src/gitr/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Control de versiones con Git");
 		System.out.println("Nueva linea");
+		System.out.println("La bebesita bblean");
 	}
 
 }
